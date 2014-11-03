1ffb0bb28e946498369bbdfd91199e68865ba12b
Maximilian Stemmer-Grabow <dev@mxsg.de>
2014-11-03 13:20:43 +0100

    Lösung a4 (Blatt 2) hinzufügen

---
+++ blatt02/a04-logelei.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Rechnernutzung in der Physik
+# WS 2014/2015
+
+# Aufgabe 4 
+# Logelei
+
+def checkNumber(a):
+    numbers = [a, 2*a]
+    # flags for each digit
+    digits = [False] * 10
+
+    for elem in numbers:
+        while elem != 0:
+            lastDigit, elem = elem % 10, elem // 10
+
+            if digits[lastDigit]:
+                return False
+            else:
+                digits[lastDigit] = True
+
+    return digits.count(True) == 10
+
+
+print "Logelei: \n"
+
+for i in range(10000, 99999):
+    if checkNumber(i):
+        print "gefunden:", i, "(=", 2*i, "/ 2)"
