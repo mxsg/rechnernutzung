e3d11a3b53867b5f2a394f077d236199eae58d60
Maximilian Stemmer-Grabow <dev@mxsg.de>
2015-01-16 19:13:49 +0100

    Materialien und Beispiele aus Repository entfernen

--- blatt03/beispiele/generate-gauss.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python -tt
-import numpy as np
-import matplotlib.pyplot as plt
- 
-#generate some Gaussion distributed Data and save to file
-data=np.random.normal(size=100)  
-np.savetxt('gaussian_numbers.dat',data)
-
